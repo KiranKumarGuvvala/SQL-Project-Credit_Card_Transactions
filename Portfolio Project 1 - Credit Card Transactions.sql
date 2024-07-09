120df3c389ebd1521d8fe65f2c5f7fe073dbb0ac
KiranKumarGuvvala <kirankumarguvvala15@gmail.com>
2024-07-09 10:47:42 +0530

    Update Portfolio Project 1 - Credit Card Transactions.sql

--- Portfolio Project 1 - Credit Card Transactions.sql
+++ Portfolio Project 1 - Credit Card Transactions.sql
@@ -1,10 +1,7 @@
 -- SQL porfolio project.
 -- download credit card transactions dataset from below link :
 -- https://www.kaggle.com/datasets/thedevastator/analyzing-credit-card-spending-habits-in-india
--- import the dataset in sql server with table name : credit_card_transcations
--- change the column names to lower case before importing data to sql server.Also replace space within column names with underscore.
--- (alternatively you can use the dataset present in zip file)
--- while importing make sure to change the data types of columns. by defualt it shows everything as varchar.
+
 
 SELECT *
 FROM credit_card_transcations;
